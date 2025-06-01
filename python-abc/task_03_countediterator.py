4b9ef04e6fb2f558c44187006152e2f23e29e7e7
Luis May <a20214976@alumnos.uady.mx>
2025-06-01 09:48:18 -0600

    Add CountedIterator class to wrap an iterator and count items iterated

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""CountedIterator class that wraps an iterator
+and counts items iterated."""
+
+
+class CountedIterator:
+    def __init__(self, iterable):
+        self.iterator = iter(iterable)
+        self.count = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        item = next(self.iterator)  # May raise StopIteration
+        self.count += 1
+        return item
+
+    def get_count(self):
+        return self.count
