360d42f657ea42ac9b40ae30ff7d0a02da395b6e
Luis May <a20214976@alumnos.uady.mx>
2025-06-08 07:54:15 -0600

    Add append_write function to append a string to a UTF-8 text file

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module provides a function that appends a string to a UTF-8 text file
+and returns the number of characters added.
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    Appends a string to the end of a UTF-8 encoded text file.
+    If the file does not exist, it is created.
+
+    Args:
+        filename (str): The name of the file.
+        text (str): The string to append.
+
+    Returns:
+        int: The number of characters added.
+    """
+    with open(filename, mode="a", encoding="utf-8") as f:
+        return f.write(text)
